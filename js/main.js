488ad16fa4126465aa623254edf8f6602efa397b
Hisham-PC <h.aljahbli@gmail.com>
2019-09-12 18:38:16 +0300

    cards flip when clicked working browser game

--- js/main.js
+++ js/main.js
@@ -30,22 +30,32 @@ function checkForMatch() {
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
             alert("You found a match!");
-            console.log("You found a match!");
+            // console.log("You found a match!");
         } else {
             alert("Sorry, try again.");
-            console.log("Sorry, try again.");
+            // console.log("Sorry, try again.");
         }
     };    
 };
 
-function flipCard(cardId) {
+function flipCard() {
+    var cardId = this.getAttribute("data-id");
+    this.setAttribute("src", cards[cardId].cardImage);
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+    // console.log(cards[cardId].cardImage);
+    // console.log(cards[cardId].suit);
     checkForMatch();
 };
 
+function createBoard() {
+  for (let index = 0; index < cards.length; index++) {
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute("src", "images/back.png");
+      cardElement.setAttribute("data-id", index);
+      cardElement.addEventListener("click", flipCard);
+      document.getElementById("game-board").appendChild(cardElement);
+  }  
+};
 
-flipCard(0);
-flipCard(2)
+createBoard();
